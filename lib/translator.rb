bd5772dda0e6c56d612d125e2ffdba7fe8f4223c
droll-accelerator-1483 <srsani@gmail.com>
2019-07-27 08:12:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,37 @@
 # require modules here
-
-def load_library
-  # code goes here
+def load_library (path)
+  require 'yaml'
+  cnf =  YAML.load_file(path)
+  cnf[:get_meaning] = [:get_meaning]
+  cnf[:get_emoticon] = [:get_emoticon]
+return cnf
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon (path, emo)
+  cnf = load_library(path)
+  out_put = []
+  for i,j in cnf
+    if j.include?(emo)
+      out_put.append(j[1])
+      return j[1]
+    end
+  end
+  if out_put ==[]
+    return ("Sorry, that emoticon was not found")
+  end
 end
 
-def get_english_meaning
-  # code goes here
+
+def get_english_meaning (path, emo)
+  cnf = load_library(path)
+  out_put = []
+  for i,j in cnf
+    if j.include?(emo)
+      out_put.append(i)
+      return i
+    end
+  end
+  if out_put ==[]
+    return ("Sorry, that emoticon was not found")
+  end
 end
